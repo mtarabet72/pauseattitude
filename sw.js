1e42b9f90ead50ecfa7602f29b1d2d69a32390ba
mtarabet72 <dreamzara76@gmail.com>
2026-07-29 22:44:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Stratégie : app shell en cache-first, contenu versionné pour permettre
 // des mises à jour propres à chaque déploiement.
 
-const CACHE_VERSION = "pa-shell-v6";
+const CACHE_VERSION = "pa-shell-v8";
 const APP_SHELL = [
   "./",
   "./index.html",
